0871a8e4262887058953306e6d94094699313e93
Gabriel Jealmo <gabrieljealmo@gmail.com>
2018-09-12 13:03:26 +0200

    Winforms Test

---
+++ ClassExcersise/FirstWindowsForm/Meddelande.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWindowsForm
+{
+    class Meddelande
+    {
+        public string Text { get; set; }
+
+        public string VisaMeddelande()
+        {
+            return "Hello World! " + Text;
+        }
+    }
+}
